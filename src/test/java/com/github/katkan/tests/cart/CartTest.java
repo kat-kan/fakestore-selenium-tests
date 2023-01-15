ee351289dfd58488bd599fbeed87a9e3c8f806b4
Katarzyna <kankowska.katarzyna@gmail.com>
2023-01-15 08:16:43 +0100

    Add first cart test (add trip from product page)

---
+++ src/test/java/com/github/katkan/tests/cart/CartTest.java
@@ -0,0 +1,45 @@
+package com.github.katkan.tests.cart;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CartTest {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeEach
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver1.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().window().maximize();
+        driver.navigate().to("https://fakestore.testelka.pl/");
+        By cookieConsentCBar = By.cssSelector(".woocommerce-store-notice__dismiss-link");
+        driver.findElement(cookieConsentCBar).click();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Verify adding product to cart from product page")
+    void addTripToCart() {
+        driver.navigate().to("https://fakestore.testelka.pl/product/fuerteventura-sotavento/");
+
+        By addToCartButton = By.cssSelector(".single_add_to_cart_button");
+        driver.findElement(addToCartButton).click();
+        By productAddedAlert = By.cssSelector(".woocommerce-message");
+        wait.until(ExpectedConditions.presenceOfElementLocated(productAddedAlert));
+        WebElement productAddedAlertElement = driver.findElement(productAddedAlert);
+
+        Assertions.assertTrue(productAddedAlertElement.getText().contains("został dodany do koszyka"));
+    }
+}
