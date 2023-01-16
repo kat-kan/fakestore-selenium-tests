5628914ddaec31c9c6cc155daaca0949dd9c407c
Katarzyna <kankowska.katarzyna@gmail.com>
2023-01-16 08:10:39 +0100

    Add 3 more tests for cart
    * Add tests for : adding trip to cart from category page, adding multiple trips from product and category page

--- src/test/java/com/github/katkan/tests/cart/CartTest.java
+++ src/test/java/com/github/katkan/tests/cart/CartTest.java
@@ -2,6 +2,7 @@ package com.github.katkan.tests.cart;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -41,5 +42,64 @@ public class CartTest {
 
         WebElement cartButtonInAlertMessage = driver.findElement(By.cssSelector(".woocommerce-message .wc-forward"));
         Assertions.assertTrue(cartButtonInAlertMessage.isDisplayed());
+    }
+
+    @Test
+    @DisplayName("Verify adding product to cart from category page")
+    void addTripToCartFromCategoryPageTest() {
+        WebElement categoriesElement = driver.findElement(By.cssSelector("div.columns-3"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", categoriesElement);
+        WebElement firstCategory = driver.findElement(By.xpath("//li[@class='product-category product first']"));
+        firstCategory.click();
+
+        driver.findElement(By.cssSelector("[data-product_id='393']")).click();
+        By addedElement = By.cssSelector(".added");
+        wait.until(ExpectedConditions.presenceOfElementLocated(addedElement));
+
+        WebElement seeCartButton = driver.findElement(By.cssSelector(".added_to_cart"));
+        Assertions.assertTrue(seeCartButton.isDisplayed());
+    }
+
+    @Test
+    @DisplayName("Verify adding minimum 10 trips to cart")
+    void add10TripsToCartTest() {
+        WebElement categoriesElement = driver.findElement(By.cssSelector("div.columns-3"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", categoriesElement);
+        WebElement firstCategory = driver.findElement(By.xpath("//li[@class='product-category product first']"));
+        firstCategory.click();
+
+        for (int i = 0; i < 10; i++) {
+            WebElement element = driver.findElement(By.cssSelector(".button[data-product_id='393']"));
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            element.click();
+            By loadingWheel = By.cssSelector(".loading");
+            wait.until(ExpectedConditions.numberOfElementsToBe(loadingWheel, 0));
+        }
+
+        WebElement cartTotalPrice = driver.findElement(By.cssSelector(".cart-contents .woocommerce-Price-amount"));
+        Assertions.assertEquals("36 000,00 zł", cartTotalPrice.getText());
+    }
+
+    @Test
+    @DisplayName("Verify adding multiple products to cart from product page")
+    void addTripToCartAndModifyAmountTest() {
+        driver.navigate().to("https://fakestore.testelka.pl/product/fuerteventura-sotavento/");
+
+        WebElement quantityField = driver.findElement(By.cssSelector("[id^='quantity']"));
+        quantityField.clear();
+        String amount = "9";
+        quantityField.sendKeys(amount);
+
+        By addToCartButton = By.cssSelector(".single_add_to_cart_button");
+        driver.findElement(addToCartButton).click();
+        By productAddedAlert = By.cssSelector(".woocommerce-message");
+        wait.until(ExpectedConditions.presenceOfElementLocated(productAddedAlert));
+
+        WebElement alert = driver.findElement(By.cssSelector(".woocommerce-message"));
+        WebElement cartButtonInAlertMessage = driver.findElement(By.cssSelector(".woocommerce-message .wc-forward"));
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(cartButtonInAlertMessage.isDisplayed()),
+                () -> Assertions.assertTrue(alert.getText().contains(amount))
+        );
     }
 }
