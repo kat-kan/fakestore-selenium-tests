794f893a5605c665e6714db67a5ab2882ded527d
Katarzyna <kankowska.katarzyna@gmail.com>
2023-01-26 00:01:31 +0100

    Create BaseTest class
    * Move @BeforeEach and @AfterEach to BaseTest

---
+++ src/test/java/com/github/katkan/tests/base/BaseTest.java
@@ -0,0 +1,28 @@
+package com.github.katkan.tests.base;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    private By cookieConsentBarLocator = By.cssSelector(".woocommerce-store-notice__dismiss-link");
+
+    @BeforeEach
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver1.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://fakestore.testelka.pl/");
+        driver.findElement(cookieConsentBarLocator).click();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
