75b26b922c5fed2c5672a8e8ca4fff58929b5e6f
Katarzyna <kankowska.katarzyna@gmail.com>
2023-02-05 15:51:14 +0100

    Add OrderTests class. Add first test skeleton

--- src/test/java/com/github/katkan/tests/order/OrderTests.java
+++ src/test/java/com/github/katkan/tests/order/OrderTests.java
@@ -1,6 +1,19 @@
 package com.github.katkan.tests.order;
 
-public class OrderTests {
+import com.github.katkan.pageObjects.OrderPage;
+import com.github.katkan.pageObjects.ProductPage;
+import com.github.katkan.tests.base.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-    
+public class OrderTests extends BaseTest {
+
+    @Test
+    @DisplayName("Check that order can be finished without creating new account")
+    void orderProductWithoutNewAccountCreationTest() {
+        String productUrl = "https://fakestore.testelka.pl/product/fuerteventura-sotavento/";
+        ProductPage productPage = new ProductPage(driver);
+        OrderPage orderPage = productPage.goTo(productUrl).addToCart().viewCart().goToCheckout();
+        orderPage.fillOrderData().fillPaymentData();
+    }
 }
