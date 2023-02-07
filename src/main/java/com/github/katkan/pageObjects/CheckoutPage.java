a3a4617a6a8ef95c96a917c967f9d2e248c543d6
Katarzyna <kankowska.katarzyna@gmail.com>
2023-02-08 00:07:38 +0100

    Add skeleton methods for singular fields in CheckoutForm

--- src/main/java/com/github/katkan/pageObjects/CheckoutPage.java
+++ src/main/java/com/github/katkan/pageObjects/CheckoutPage.java
@@ -7,7 +7,6 @@ public class CheckoutPage extends BasePage {
 
     private By firstNameFieldLocator = By.cssSelector("#billing_first_name");
     private By lastNameFieldLocator = By.cssSelector("#billing_last_name");
-    private By companyFieldLocator = By.cssSelector("#billing_company");
     private By countryFieldLocator = By.cssSelector("#billing_country");
     private By addressFieldLocator = By.cssSelector("#billing_address_1");
     private By postcodeFieldLocator = By.cssSelector("#billing_postcode");
@@ -35,6 +34,54 @@ public class CheckoutPage extends BasePage {
 
     public CheckoutPage fillOrderData() {
         //TODO implement method
+        return new CheckoutPage(driver);
+    }
+
+    public CheckoutPage addFirstName(String firstName){
+        //TODO check if for logged in user clear is needed
+        driver.findElement(firstNameFieldLocator).sendKeys(firstName);
+        return new CheckoutPage(driver);
+    }
+
+    public CheckoutPage addLastName(String lastName){
+        //TODO check if for logged in user clear is needed
+        driver.findElement(lastNameFieldLocator).sendKeys(lastName);
+        return new CheckoutPage(driver);
+    }
+
+    public CheckoutPage addCountry(String country){
+        //TODO check if for logged in user clear is needed
+        driver.findElement(countryFieldLocator).sendKeys(country);
+        return new CheckoutPage(driver);
+    }
+
+    public CheckoutPage addAddress(String address){
+        //TODO check if for logged in user clear is needed
+        driver.findElement(addressFieldLocator).sendKeys(address);
+        return new CheckoutPage(driver);
+    }
+
+    public CheckoutPage addPostcode(String postcode){
+        //TODO check if for logged in user clear is needed
+        driver.findElement(postcodeFieldLocator).sendKeys(postcode);
+        return new CheckoutPage(driver);
+    }
+
+    public CheckoutPage addCity(String city){
+        //TODO check if for logged in user clear is needed
+        driver.findElement(cityFieldLocator).sendKeys(city);
+        return new CheckoutPage(driver);
+    }
+
+    public CheckoutPage addEmail(String email){
+        //TODO check if for logged in user clear is needed
+        driver.findElement(emailFieldLocator).sendKeys(email);
+        return new CheckoutPage(driver);
+    }
+
+    public CheckoutPage addPhone(String phone){
+        //TODO check if for logged in user clear is needed
+        driver.findElement(phoneNumberFieldLocator).sendKeys(phone);
         return new CheckoutPage(driver);
     }
 
