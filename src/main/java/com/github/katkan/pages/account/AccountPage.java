324a43df5be38dc3fb345032264902b162b2edfe
Katarzyna <kankowska.katarzyna@gmail.com>
2023-04-15 23:19:24 +0200

    Fix conflicts in AccountPage

--- src/main/java/com/github/katkan/pages/account/AccountPage.java
+++ src/main/java/com/github/katkan/pages/account/AccountPage.java
@@ -2,8 +2,10 @@ package com.github.katkan.pages.account;
 
 import com.github.katkan.pages.main.BasePage;
 import com.github.katkan.pages.tables.my_orders.MyOrdersTable;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,30 +13,28 @@ public class AccountPage extends BasePage {
 
     private WebDriverWait wait;
 
-    private By myOrdersButtonLocator = By.cssSelector("[class$='--orders']");
-    private By deleteButtonLocator = By.cssSelector(".delete-me");
+    @FindBy(css = "[class$='--orders']")
+    WebElement myOrdersButton;
+
+    @FindBy(css = ".delete-me")
+    WebElement deleteButton;
 
     public AccountPage(WebDriver driver) {
         super(driver);
         wait = new WebDriverWait(driver, 5);
+        PageFactory.initElements(driver, this);
     }
 
-    public void deleteAccount(){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(deleteButtonLocator));
-        driver.findElement(deleteButtonLocator).click();
+    public void deleteAccount() {
+        wait.until(ExpectedConditions.visibilityOf(deleteButton));
+        deleteButton.click();
         driver.switchTo().alert().accept();
         wait.until(ExpectedConditions.urlToBe("https://fakestore.testelka.pl/"));
     }
 
-/*    public MyOrdersPage viewOrders(){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(myOrdersButtonLocator));
-        driver.findElement(myOrdersButtonLocator).click();
-        return new MyOrdersPage(driver);
-    }*/
-
     public MyOrdersTable viewOrders(){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(myOrdersButtonLocator));
-        driver.findElement(myOrdersButtonLocator).click();
+        wait.until(ExpectedConditions.visibilityOf(myOrdersButton));
+        myOrdersButton.click();
         return new MyOrdersTable(driver);
     }
 }
