5c3f8467a38826b11a74f684039d7fe568ab6b5b
Katarzyna <kankowska.katarzyna@gmail.com>
2023-04-15 23:08:37 +0200

    Add new classes to handle My Orders table
    *Add MyOrdersModel class -> model for order in MyOrders table
    * Add MyOrdersRow class to with column locators and toModel method which builds MyOrdersModel object
    * Add MyOrdersTable class with table and rows locators and methods: initializeAllOrders() which creates ArrayList of MyOrderModel objects from List of WebElements representing table rows; getOrder(String id) and navigateToOrder(String id)

---
+++ src/main/java/com/github/katkan/pages/tables/my_orders/MyOrdersTable.java
@@ -0,0 +1,49 @@
+package com.github.katkan.pages.tables.my_orders;
+
+import com.github.katkan.models.MyOrdersModel;
+import com.github.katkan.pages.account.OrderDetailsPage;
+import com.github.katkan.pages.main.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class MyOrdersTable extends BasePage {
+
+    @FindBy(css = ".woocommerce-orders-table")
+    private WebElement ordersTableContainer;
+
+    @FindBy(css = ".woocommerce-orders-table__row")
+    private List<WebElement> rows;
+
+    private final List<MyOrdersModel> myOrdersList = new ArrayList<>();
+
+    public MyOrdersTable(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(new DefaultElementLocatorFactory(ordersTableContainer), this);
+        initializeAllOrders();
+    }
+
+    public OrderDetailsPage navigateToOrder(String id) {
+        getOrder(id).getOrderLink().click();
+        return new OrderDetailsPage(driver);
+    }
+
+    public MyOrdersModel getOrder(String id) {
+        return myOrdersList.stream()
+                .filter(myOrdersModel -> myOrdersModel.getOrderLink().getText().contains(id))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Order with id " + id + "not found"));
+    }
+
+    private void initializeAllOrders() {
+        rows.stream()
+                .parallel()
+                .forEach(row -> myOrdersList.add(new MyOrdersRow(driver, row).toModel()));
+    }
+}
