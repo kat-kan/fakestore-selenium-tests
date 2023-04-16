b58d41f690091020a774bf2b24e874a3296705cf
Katarzyna <kankowska.katarzyna@gmail.com>
2023-04-16 22:26:58 +0200

    Rewrite Header and Footer Pages to use Page Factory pattern
    *Add @FindBy annotations for web elements
    *Modify methods

--- src/main/java/com/github/katkan/pages/main/FooterPage.java
+++ src/main/java/com/github/katkan/pages/main/FooterPage.java
@@ -1,17 +1,19 @@
 package com.github.katkan.pages.main;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class FooterPage extends BasePage {
 
-    private By cookieConsentBarLocator = By.cssSelector(".woocommerce-store-notice__dismiss-link");
+    @FindBy(css = ".woocommerce-store-notice__dismiss-link")
+    private WebElement cookieConsentBar;
 
     public FooterPage(WebDriver driver) {
         super(driver);
     }
 
     public void closeCookieConsentBar() {
-        driver.findElement(cookieConsentBarLocator).click();
+        cookieConsentBar.click();
     }
 }
--- src/main/java/com/github/katkan/pages/main/HeaderPage.java
+++ src/main/java/com/github/katkan/pages/main/HeaderPage.java
@@ -1,26 +1,30 @@
 package com.github.katkan.pages.main;
 
-import com.github.katkan.pages.cart.CartPage;
 import com.github.katkan.pages.account.AccountPage;
-import org.openqa.selenium.By;
+import com.github.katkan.pages.cart.CartPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HeaderPage extends BasePage {
+
+    @FindBy(css = "a.cart-contents")
+    private WebElement totalPriceInCart;
+
+    @FindBy(css = "#menu-item-201")
+    private WebElement myAccount;
 
     public HeaderPage(WebDriver driver) {
         super(driver);
     }
 
-    private By totalPriceLocator = By.cssSelector("a.cart-contents");
-    private By myAccountLocator = By.cssSelector("#menu-item-201");
-
     public CartPage viewCart(){
-        driver.findElement(totalPriceLocator).click();
+        totalPriceInCart.click();
         return new CartPage(driver);
     }
 
     public AccountPage viewMyAccount(){
-        driver.findElement(myAccountLocator).click();
+        myAccount.click();
         return new AccountPage(driver);
     }
 }
