e7941cffee42da445c9ee3a0a7384051a20da124
Katarzyna <kankowska.katarzyna@gmail.com>
2023-04-21 23:27:30 +0200

    Add CartItemModel class to represent products in the cart

---
+++ src/main/java/com/github/katkan/models/CartItemModel.java
@@ -0,0 +1,18 @@
+package com.github.katkan.models;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.openqa.selenium.WebElement;
+
+@Builder
+@Getter
+@Setter
+public class CartItemModel {
+    private WebElement removeButton;
+    private WebElement thumbnail;
+    private WebElement productLink;
+    private String price;
+    private String quantity;
+    private String totalPrice;
+}
