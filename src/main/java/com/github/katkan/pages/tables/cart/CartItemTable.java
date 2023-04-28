c4422911ca94d3095306df5e1b97a5f4ad563bab
Katarzyna <kankowska.katarzyna@gmail.com>
2023-04-28 08:37:50 +0200

    Add fitering method in CartItemTable (by product name)

--- src/main/java/com/github/katkan/pages/tables/cart/CartItemTable.java
+++ src/main/java/com/github/katkan/pages/tables/cart/CartItemTable.java
@@ -1,7 +1,6 @@
 package com.github.katkan.pages.tables.cart;
 
 import com.github.katkan.models.CartItemModel;
-import com.github.katkan.pages.cart.CartPage;
 import com.github.katkan.pages.main.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,8 +8,9 @@ import org.openqa.selenium.support.FindBy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-public class CartItemTable extends CartPage {
+public class CartItemTable extends BasePage {
 
     @FindBy(css = ".woocommerce-cart-form__contents")
     private WebElement table;
@@ -29,5 +29,12 @@ public class CartItemTable extends CartPage {
         rows.stream()
                 .parallel()
                 .forEach(r-> cartItems.add(new CartItemRow(driver).toModel()));
+    }
+
+    public CartItemModel getCartItem(String name){
+        return cartItems.stream()
+                .filter(cartItem-> cartItem.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Product " + name + " not found"));
     }
 }
