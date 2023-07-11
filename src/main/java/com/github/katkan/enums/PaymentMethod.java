91b506b505c562397a41fd18a063df42e6475027
Katarzyna <kankowska.katarzyna@gmail.com>
2023-07-11 18:58:19 +0200

    Replace constructor with Lombok annotation

--- src/main/java/com/github/katkan/enums/PaymentMethod.java
+++ src/main/java/com/github/katkan/enums/PaymentMethod.java
@@ -1,15 +1,14 @@
 package com.github.katkan.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public enum PaymentMethod {
     CARD("Karta debetowa/kredytowa (Stripe)");
 
     private final String description;
 
-    PaymentMethod(String description) {
-        this.description = description;
-    }
 }
 
